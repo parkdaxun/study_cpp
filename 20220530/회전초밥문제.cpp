52326bbcafd2d9908281503130263d298073df4d
parkdaxun <parkde0207@gmail.com>
2022-05-30 16:10:54 +0900

    2022.05.30 수업3

--- 20220530/회전초밥문제.cpp
+++ 20220530/회전초밥문제.cpp
@@ -1,35 +1,63 @@
 #include <iostream>
 using namespace std;
+#defin MAX_BUDGET 1000000
+int n;
+int m;
+int price[20];
+int preference[20];
 
-int arr[20][2]={0}, n, money;
+int FindMaxPref_recursive(int target)
+{
+    int maxPref = 0;
+    for(int sushi = 0; sushi<n; sushi++)
+    {
+        if(budget - price[sushi] >= 0)
+        {
+            maxPref=max(maxPref, FindMaxPref_recursive(budget-price[sushi])+preference[sushi]);
+        }
+    }
+    return maxPref;
+}
 
-int check(int i) {
-	
-	int nmoney=0, sum=0;
-	
-	for(int z=i; z<n; z++) {
-		if(nmoney >= money) return sum;
-		nmoney += arr[z][0];
-		sum += arr[z][1];
-	}
-	return sum;
+
+
+int cacke[MAX_BUDGET+1];
+int FindMaxPref_repeat()
+{
+    cacke[0]=0;
+    for(int budget=1; budget<=m; budget++)
+    {
+        cache[budget]=0;
+        for(int sushi = 0; sushi<n; sushi++)
+        {
+            int targetPrice = price[sushi];
+            int targetPref = preference[sushi];
+            if(budget<targetPrice) continue;
+            cacke[budget] = max(cacke[budget], cacke[budget-targetPrice]+targetPref);
+        }
+    }
+    return cache[m];
+}
+
+int sw[201];
+int FindMaxPref_sliding_widow()
+{
+    m=m/100;
+    sw[0]=0;
+    for(int budget=1; budget<=m; budget++)
+    {
+        sw[budget%201] = 0;
+        for(int sushi=0; sushi<n; sushi++)
+        {
+            int targetPrice = price[sushi]/100;
+            int targetPref = preference[sushi];
+            if(budget<targetPrice) continue;
+            sw[budget%201] = max(sw[budget%201], sw[(budget-targetPrice)%201]+targetPref);
+        }
+    }
+    return sw[m%201];
 }
 
 int main() {
-	int max=0;
-	
-	cin>>n>>money;
-	for(int i=0; i<n; i++) {
-		for(int j=0; j<2; j++)
-			cin>>arr[i][j];
-	}
-	
-	int temp;
-	for(int i=0; i<n; i++) {
-		temp = check(i);
-		if(temp > max) max = temp;
-	}
-	
-	cout << max;
-	return 0;
+    
 }
