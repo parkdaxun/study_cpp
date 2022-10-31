f94f003f5f3e63e0447af2adb1e7eec3f0287ae4
parkdaxun <parkde0207@gmail.com>
2022-10-31 14:02:29 +0900

    20221031 수업

--- 2학기/20221024/Monster.h
+++ 2학기/20221024/Monster.h
@@ -2,52 +2,45 @@
 #define __MONSTER_H__
 
 #include <string>
+#include <cstdlib>
+#include <ctime>
 #include "Canvas.h"
 
 class Monster {
-private:
-	string name;
-	char icon;
-	int x, y, nItem;
-private:
-	void clip(int xMax, int yMax) {
-		if(x < 0) x = 0;
-		if(x >= xMax) x = xMax - 1;
-		if(y < 0) y = 0;
-		if(y >= yMax) y = yMax - 1;
-	}
-	void eat(int map[DIM][DIM]) {
-		if(map[y][x] == 1) {
-			map[y][x] = 0;
-			nItem++;
-			}
-	}
+protected:
+    string name;
+    char icon;
+    int x, y, nItem;
+
+    void clip(int xMax, int yMax) {
+      if(x < 0) x = 0;
+      if(x >= xMax) x = xMax - 1;
+      if(y < 0) y = 0;
+      if(y >= xMax) y = yMax - 1;
+    }
+
+    void eat(int map[DIM][DIM]) {
+      if(map[y][x] == 1) {
+        map[y][x] = 0;
+        nItem++;
+      }
+    }
 public:
-	Monster(string name="괴물", char icon='@', int x=0, int y=0):
-		name(name), icon(icon), x(x), y(y), nItem(0) {}
-	~Monster() {
-		cout << "\t" << name << icon << " 퇴장합니다~~\n"; 
-	}
-	void draw(Canvas& canvas) {
-		canvas.draw(x, y, icon);
-	}
-	void move(int map[DIM][DIM], int xMax, int yMax) {
-		switch(rand()%8) {
-			case 0: y--; break; // up
-			case 1: x++; y--; break; // right up
-			case 2: x++; break; // right
-			case 3: x++; y++; break; // right down
-			case 4: y++; break; // down
-			case 5: x--; y++; break; // left down
-			case 6: x--; break; // left
-			case 7: x--; y--; break; // left up
-		}
-		clip(xMax, yMax);
-		eat(map);
-	}
-	void print() {
-		cout << "\t" << name << icon << ":" << nItem << endl;
-	}
+    Monster(string name = "±´π∞", char icon = '@', int x = 0, int y = 0) : name(name), icon(icon), x(x), y(y), nItem(0) {}
+
+    virtual ~Monster() {
+      cout << name << icon << " ≈¿Â«’¥œ¥Ÿ~~" << endl;
+    }
+
+    void draw(Canvas& canvas) {
+      canvas.draw(x, y, icon);
+    }
+    
+    virtual void move(int map[DIM][DIM], int xMax, int yMax) = 0;
+
+    void print() {
+      cout << name << icon << ":" << nItem << endl;
+    }
 };
 
 #endif
